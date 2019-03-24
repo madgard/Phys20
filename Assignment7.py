3eeab0bd3fa5521a170978867acaae891b6eb7d1
mgardner <mgardner@caltech.edu>
2019-03-24 10:05:32 -0700

    More clean-up

--- Assignment7.py
+++ Assignment7.py
@@ -1,4 +1,4 @@
-from math import log, log10, sqrt
+from math import log, log10, sqrt, sin, sinh
 from scipy.constants import c
 import numpy as np
 import matplotlib.pyplot as mpl
@@ -10,43 +10,38 @@ def distance_modulus_to_luminosity_distance(distance, uncertainty):
     d_Mpc = d_parsecs/10**6
     dd_Mpc = dd_parsecs/10**6
     return d_Mpc, dd_Mpc
-# def luminosity_distance_to_distance_modulus(distance):
-#     d_parsecs = distance*10**6
-#     distance_modulus = [5*(log10(d)-1) for d in d_parsecs]
-#     return distance_modulus
 def Hubbles_Law(H0, redshift):
     luminosity_distance = c*redshift/H0
     return luminosity_distance
 def non_Linear_Hubbles_Law(redshift, H0, q):
     luminosity_distance = c*redshift/H0*(1 + ((1 - q)/2)*redshift)
     return luminosity_distance
-# def H02(luminosity_distance, redshift, H0):
-#     calc_lum_dist = Hubbles_Law(redshift, H0)
-#     differences = luminosity_distance-calc_lum_dist
-#     the_fraction = differences**2/calc_lum_dist
-#     X2 = np.sum(the_fraction)
-#     return X2
-# def X2(luminosity_distance, redshift, H0, q):
-#     calc_lum_dist = non_Linear_Hubbles_Law(redshift, H0, q)
-#     differences = luminosity_distance-calc_lum_dist
-#     the_fraction = differences**2/calc_lum_dist
-#     X2 = np.sum(the_fraction)
-#     return X2
 def integrate(z, Ohm_r, Ohm_m, Ohm_k, Ohm_A):
     integrate_this = 1/(Ohm_r*(1+z)**4+Ohm_m*(1+z)**3+Ohm_k*(1+z)**2+Ohm_A)
     return integrate_this
-def FLRW(redshift,H0,Ohm_m,Ohm_k,Ohm_r):
+def FLRW(redshift,H0,Ohm_m,Ohm_r):
     d_H = c/H0
-    Ohm_A = 1 - Ohm_m - Ohm_k
-    integral = np.array([quad(integrate, 0, z, args=(Ohm_r, Ohm_m, Ohm_k, Ohm_A,))[0] for z in redshift])
+    Ohm_A = 1 - Ohm_m
+    integral = np.array([quad(integrate, 0, z, args=(Ohm_r, Ohm_m, 0, Ohm_A,))[0] for z in redshift])
     d_C = d_H*integral
     d_M = d_C
     d_L = (1+redshift)*d_M
     return d_L
-def Ohm_k_0(redshift,H0,Ohm_m,Ohm_r):
-    return FLRW(redshift,H0,Ohm_m,0,Ohm_r)
-#def best_q(H0, redshift, luminosity_distance):
-#    for
+def FLRW2(redshift,H0,Ohm_m,Ohm_A,Ohm_r):
+    d_H = c/H0
+    Ohm_k = 1 - Ohm_m - Ohm_A
+    integral = np.array([quad(integrate, 0, z, args=(Ohm_r, Ohm_m, Ohm_k, Ohm_A,))[0] for z in redshift])
+    d_C = d_H*integral
+    if Ohm_k < 0:
+        d_M = [d_H/sqrt(abs(Ohm_k))*sin(sqrt(abs(Ohm_k))*dC/d_H) for dC in d_C]
+    elif Ohm_k > 0:
+        d_M = [d_H/sqrt(Ohm_k)*sinh(sqrt(Ohm_k)*dC/d_H) for dC in d_C]
+    elif Ohm_k == 0:
+        d_M = d_C
+    d_L = (1+redshift)*d_M
+    return d_L
+
+#1: parse the file into relevant arrays, convert to luminosity distance
 f = open("SCPUnion2.1_mu_vs_z.txt")
 lines=f.readlines()
 col1 = np.empty(0)
@@ -62,7 +57,6 @@ for i in range(len(lines)):
         col3 = np.append(col3,float(values[2]))
         col4 = np.append(col4,float(values[3]))
         col5 = np.append(col5,float(values[4]))
-#1: parse the file into relevant arrays, convert to luminosity distance
 dists = col3
 reds = col2
 uncs = col4
@@ -97,7 +91,6 @@ for i in range(len(reds)):
 H0_lin = np.mean(H0s)
 predicted_hubble = Hubbles_Law(H0_lin, reds)
 difference = lum_dists-predicted_hubble
-# X2 = H02(lum_dists, reds, H0_lin)
 print("H0:", H0_lin)
 
 #4: Plot with our linear regression with our H0
@@ -106,7 +99,6 @@ mpl.scatter(reds, lum_dists, s = 1, color = 'red')
 mpl.plot(reds, predicted_hubble, linewidth = 1, color = 'black')
 mpl.xlabel('redshift')
 mpl.ylabel('luminosity distance')
-# mpl.show()
 mpl.subplot(122)
 mpl.scatter(predicted_hubble, difference/lum_uncs, s = 1, color = 'purple')
 mpl.xlabel('predicted')
@@ -127,13 +119,7 @@ mpl.scatter(reds, lum_dists, s = 1, color = 'red')
 mpl.scatter(points, calculated_with_deceleration, s = 1, color = 'black')
 mpl.xlabel('redshift')
 mpl.ylabel('luminosity distance')
-# mpl.show()
 mpl.subplot(122)
-# mpl.scatter(reds, dists, s = 1, color = 'red')
-# mpl.scatter(points, luminosity_distance_to_distance_modulus(calculated_with_deceleration), s = 1, color = 'black')
-# mpl.xlabel('redshift')
-# mpl.ylabel('distance modulus')
-# mpl.show()
 #residuals
 predicted_non_linear = non_Linear_Hubbles_Law(reds, H0_deceleration_scipy, q)
 difference1 = lum_dists - predicted_non_linear
@@ -146,55 +132,50 @@ mpl.show()
 #6. Fit to our data with the FLRW via the curve-fit sci-py method.
 #   Plot this fit and the normalized residuals.
 Ohm_k = 0
-parameters2, param_Covariance2 = curve_fit(Ohm_k_0, reds, lum_dists, sigma = lum_uncs)
+parameters2, param_Covariance2 = curve_fit(FLRW, reds, lum_dists, sigma = lum_uncs)
 H0_FLRW_scipy, Ohm_m, Ohm_r = parameters2
-calculated_with_FLRW = FLRW(points, H0_FLRW_scipy, Ohm_m, Ohm_k, Ohm_r)
+calculated_with_FLRW = FLRW(points, H0_FLRW_scipy, Ohm_m, Ohm_r)
 mpl.subplot(121)
 mpl.scatter(reds, lum_dists, s = 1, color = 'red')
 mpl.scatter(points, calculated_with_FLRW, s = 1, color = 'black')
 mpl.xlabel('redshift')
 mpl.ylabel('luminosity distance')
-# mpl.show()
 mpl.subplot(122)
-# mpl.scatter(reds, dists, s = 1, color = 'red')
-# mpl.scatter(points, luminosity_distance_to_distance_modulus(calculated_with_FLRW), s = 1, color = 'black')
-# mpl.xlabel('redshift')
-# mpl.ylabel('distance modulus')
-# mpl.show()
 ###residuals
-predicted_FLRW = FLRW(reds, H0_FLRW_scipy, Ohm_m, Ohm_k, Ohm_r)
+predicted_FLRW = FLRW(reds, H0_FLRW_scipy, Ohm_m, Ohm_r)
 difference2 = lum_dists - predicted_FLRW
 norm_resid2 = difference2/lum_uncs
-mpl.scatter(predicted_FLRW, norm_resid2, s = 1, color = 'purple')#/(lum_dists*)param_Covariance2[0,1]
+mpl.scatter(predicted_FLRW, norm_resid2, s = 1, color = 'purple')
 mpl.xlabel('predicted')
 mpl.ylabel('normalized residuals')
 mpl.show()
 print ("H0, Ohm_m, Ohm_r:", parameters2)
-#The statistical significance of Ohm_m and therefore Ohm_A.
+
+#7. The statistical significance of Ohm_m and therefore Ohm_A.
 print('Ohm_A: ', 1-Ohm_m, 'Covariance Ohm_m: ', sqrt(param_Covariance2[1,1]))
 # Without using radiation: ('Ohm_A: ', 0.8854955179779508, 'Covariance Ohm_m: ', 0.0001755520748644085)
 # With radiation: ('Ohm_A: ', 0.7113206558831211, 'Covariance Ohm_m: ', 0.12957568504213926)
-# radiation changes the significance, so it is significant
+# radiation definitely changes things, so include it
 
 #Again, but the universe is curved.
 del Ohm_k
-parameters21, param_Covariance21 = curve_fit(FLRW, reds, lum_dists, sigma = lum_uncs)
-H0_FLRW_scipy, Ohm_m, Ohm_k, Ohm_r = parameters21
-calculated_with_FLRW = FLRW(points, H0_FLRW_scipy, Ohm_m, Ohm_k, Ohm_r)
+parameters21, param_Covariance21 = curve_fit(FLRW2, reds, lum_dists, sigma = lum_uncs, bounds = ((-np.inf,0,0,0),(np.inf,np.inf,np.inf,np.inf)))
+H0_FLRW_scipy, Ohm_m, Ohm_A, Ohm_r = parameters21
+calculated_with_FLRW = FLRW2(points, H0_FLRW_scipy, Ohm_m, Ohm_A, Ohm_r)
 mpl.subplot(121)
 mpl.scatter(reds, lum_dists, s = 1, color = 'red')
 mpl.scatter(points, calculated_with_FLRW, s = 1, color = 'black')
 mpl.xlabel('redshift')
 mpl.ylabel('luminosity distance')
 mpl.subplot(122)
-predicted_FLRW = FLRW(reds, H0_FLRW_scipy, Ohm_m, Ohm_k, Ohm_r)
+predicted_FLRW = FLRW2(reds, H0_FLRW_scipy, Ohm_m, Ohm_A, Ohm_r)
 difference21 = lum_dists - predicted_FLRW
 norm_resid21 = difference21/lum_uncs
 mpl.scatter(predicted_FLRW, norm_resid21, s = 1, color = 'purple')
 mpl.xlabel('predicted')
 mpl.ylabel('normalized residuals')
 mpl.show()
-print ("H0, Ohm_m, Ohm_r:", parameters21)
-#The statistical significance of Ohm_m and therefore Ohm_A.
-print('Ohm_A: ', 1-Ohm_m-Ohm_k, 'Covariance Ohm_m: ', sqrt(param_Covariance21[1,1])+sqrt(param_Covariance21[2,2]))
+print ("H0, Ohm_m, Ohm_A, Ohm_r, Ohm_k:", parameters21, 1-Ohm_A-Ohm_m)
+#7: Ohm_A and the uncertainty in it.
+print('Ohm_A: ', Ohm_A, 'Uncertainty in Ohm_A: ', sqrt(param_Covariance21[2,2]))
 f.close()
